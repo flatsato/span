4629f5a2fac84d78fad8af9d41497342a92ab7df
FLAT サトウ ハルミ <sato@wd-flat.com>
2017-05-04 14:11:08 +0900

    最新に修正＜gulpfile.js、package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,28 @@
-var $, gulp, pngquant;
+var $, gulp, pngquant, bulkSass;
 gulp = require('gulp');
 pngquant = require('imagemin-pngquant');
 $ = require('gulp-load-plugins')();
+bulkSass = require('gulp-sass-bulk-import');
 
-//
 //    gulp-webserver【1】LiveReload環境構築
 //    gulp-file-include【2】HTMLインクルード
-//    gulp-autoprefixer【3】autoprefixer追加
-//    gulp-cssmin【4】CSS圧縮
-//    gulp-uglify【5】JavaScript圧縮
-//    gulp-imagemin【6】img圧縮
-//    gulp-sourcemaps 【8】sourcemap作成
+//    gulp-sass【3】sass
+//    gulp-autoprefixer【4】autoprefixer追加
+//    gulp-cssmin【5】css圧縮
+//    gulp-uglify【6】JavaScript圧縮
+//    gulp-imagemin【7】img圧縮
+//    gulp-htmlmin【8】HTML圧縮
+//    gulp-sourcemaps 【9】sourcemap作成
+//    gulp.spritesmith【10】sprite画像
 
 //    gulp
 //    gulp-sass Sassコンパイル
 //    gulp-notify エラーを通知
 //    gulp-load-plugins パッケージを読み込み
 //    gulp-plumber エラーが出たときにgulpを終了させない
-//    gulp-rename 
+//    gulp-rename
 //    imagemin-pngquant png圧縮
+//    gulp-sass-bulk-import sass import 一括読み込み
 
 //gulp-webserver【1】LiveReload環境構築
 gulp.task('webserver', function () {
@@ -36,25 +40,31 @@ gulp.task('html', function () {
   })).pipe($.fileInclude({
     prefix: '@@',
     basepath: './src/template/_include/'
-  })).pipe(gulp.dest('./dist/'));
+  })).pipe(gulp.dest('./src/_html-original/'));
 });
 
 //gulp-autoprefixer【3】autoprefixer追加
-var autoprefixer = require("gulp-autoprefixer");
-gulp.task("auto", function () {
+//gulp-sourcemaps 【8】sourcemap作成
+var autoprefixer = require("gulp-autoprefixer"),
+  sass = require('gulp-sass'),
+  sourcemaps = require('gulp-sourcemaps');
+gulp.task("sass", function () {
   gulp.src("./src/css/**/*scss")
+    .pipe(bulkSass())
+    .pipe(sourcemaps.init())
     .pipe(sass())
-    .pipe(autoprefixer())
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions', 'ie 9', 'android 4.0']
+    }))
+    .pipe(sourcemaps.write('./maps'))
     .pipe(gulp.dest("./dist/css"));
 });
 
 //gulp-cssmin【4】css圧縮
-var gulp = require('gulp');
 var cssmin = require('gulp-cssmin');
 var rename = require('gulp-rename');
-
 gulp.task('cssmin', function () {
-  gulp.src('./dist/**/*.css')
+  gulp.src('./dist/**/style.css')
     .pipe(cssmin())
     .pipe(rename({
       suffix: '.min'
@@ -62,9 +72,8 @@ gulp.task('cssmin', function () {
     .pipe(gulp.dest('dist'));
 });
 
-//gulp-uglify【5】JavaScript圧縮
+//gulp-uglify【6】JavaScript圧縮
 var uglify = require("gulp-uglify");
-
 gulp.task("js", function () {
   gulp.src(["./src/js/**/*.js", "!./src/js/vendor/*.js"])
     .pipe(uglify())
@@ -76,7 +85,7 @@ gulp.task("js", function () {
     .pipe(gulp.dest("./dist/js/vendor"));
 });
 
-//gulp-imagemin【6】img圧縮
+//gulp-imagemin【7】img圧縮
 gulp.task('image', function () {
   return gulp.src('./src/img/**/*').pipe($.imagemin({
     progressive: true,
@@ -85,25 +94,45 @@ gulp.task('image', function () {
   })).pipe(gulp.dest('./dist/img/'));
 });
 
-//gulp-sourcemaps 【8】sourcemap作成
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
+//gulp-htmlmin【8】HTML圧縮
+var htmlmin = require('gulp-htmlmin');
+gulp.task('minify', function () {
+  return gulp.src('./src/_html-original/**/*.html')
+    .pipe(htmlmin({
+      collapseWhitespace: true
+    }))
+    .pipe(gulp.dest('./dist/'))
+});
 
-gulp.task('sass', function () {
-  return gulp.src('./src/css/**/*.scss')
-    .pipe(sourcemaps.init())
-    .pipe(sass())
-    .pipe(sourcemaps.write('./dist/css/maps'))
-    .pipe(gulp.dest('./dist/css/'));
+//gulp.spritesmith【10】sprite画像
+//http://blog.e-riverstyle.com/2014/02/gulpspritesmithcss-spritegulp.html
+// Android 4.2〜対応とするため SPのspriteは無し
+var spritesmith = require('gulp.spritesmith');
+gulp.task('sprite', function () {
+  var spriteData = gulp.src('./src/img/sprite/*.png') //スプライトにする画像達
+    .pipe(spritesmith({
+      imgName: 'sprite.png', //スプライトの画像
+      cssName: '_sprite.scss', //生成されるscss
+      imgPath: '../img/sprite.png', //生成されるscssに記載されるパス
+      cssFormat: 'scss', //フォーマット
+      cssVarMap: function (sprite) {
+        sprite.name = 'sprite-' + sprite.name; //VarMap(生成されるScssにいろいろな変数の一覧を生成)
+      }
+    }));
+  spriteData.img.pipe(gulp.dest('./dist/img/')); //imgNameで指定したスプライト画像の保存先
+  spriteData.css.pipe(gulp.dest('./src/css/')); //cssNameで指定したcssの保存先
+});
+
+gulp.task('default', function () {
+  gulp.run('sprite');
 });
 
 gulp.task('watch', function () {
   gulp.watch('./src/template/**/*.html', ['html']);
   gulp.watch('./src/img/**/*', ['image']);
   gulp.watch('./src/css/**/*.scss', ['sass']);
-  gulp.watch('./src/css/**/*.scss', ['auto']);
   gulp.watch(["./src/js/**/*.js", "!./dist/js/**/*.js"], ["js"]);
 });
 
-gulp.task('default', ['html', 'auto', 'image', 'webserver', 'js', 'watch']);
+gulp.task('default', ['webserver', 'html', 'sass', 'cssmin', 'js', 'image', 'minify',
+  'sprite', 'watch']);
